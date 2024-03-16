3af993e85ec83286e4be7ccf67ab671dfaf4c316
Esteban Ibarra <sectorjeat@gmail.com>
2024-03-16 15:00:33 -0600

    Adds Go and Nodejs configs

--- .config/shell/userconfig.sh
+++ .config/shell/userconfig.sh
@@ -1,21 +1,17 @@
-# ========================== SHELL USER CONFIGURATION ==========================
+# ========================== SHELL USER CONFIGURATION =========================
 # Use this file to set your user configurations such as environment variables
 # and paths.
+# =============================================================================
+# If you need to set environment variables with sensitive information, you can
+# create a file called .env in the ~/.config/shell directory and set the
+# variables there. This file will be sourced if it exists.
 
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
+export LANG=en_US.UTF-8
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$(go env GOPATH)/bin
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 if [ -f ~/.config/shell/.env ]; then
   source ~/.config/shell/.env

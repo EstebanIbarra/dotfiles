057b450f909eb8aa93722950c25652311b3793eb
Esteban Ibarra <sectorjeat@gmail.com>
2024-03-17 09:08:37 -0600

    Adds pyenv config

--- .config/shell/userconfig.sh
+++ .config/shell/userconfig.sh
@@ -7,12 +7,19 @@
 # variables there. This file will be sourced if it exists.
 
 export LANG=en_US.UTF-8
+# Go
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$(go env GOPATH)/bin
+# Node
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Python
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
+# Source .env file
 if [ -f ~/.config/shell/.env ]; then
   source ~/.config/shell/.env
 fi
